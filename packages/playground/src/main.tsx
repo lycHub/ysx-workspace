2d12c9feb71bfa233504b734b17dbbf2513e573b
yichao.li.ext <yichao.li.ext@zeiss.com>
2023-03-14 10:15:28 +0800

    fetch chat

--- packages/playground/src/main.tsx
+++ packages/playground/src/main.tsx
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-console.log('loggggggg');
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
+
+const myRequest = new Request('https://api.openai.com/v1/chat/completions', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer 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'
+  },
+  body: JSON.stringify({
+    "model": "gpt-3.5-turbo",
+    "messages": [{"role": "user", "content": "Hello!"}]
+  })
+});
+fetch(myRequest).then(response => response.json())
+.then(json => console.log(json));
