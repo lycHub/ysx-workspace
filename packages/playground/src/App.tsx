a1745a0ebcf7bb1141596a94a14b71e161aaab03
lycHub <1120805364@qq.com>
2023-04-01 14:36:29 +0800

    sprint

--- packages/playground/src/App.tsx
+++ packages/playground/src/App.tsx
@@ -1,10 +1,18 @@
 import React  from "react";
 
 import "./App.scss";
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useLocation, useOutlet } from "react-router-dom";
+import { animated, useTransition } from "@react-spring/web";
 
 function App() {
-
+	const location = useLocation();
+	const currentOutlet = useOutlet();
+	const transitions = useTransition(location.pathname, {
+		exitBeforeEnter: true,
+		from: { opacity: 0 },
+		enter: { opacity: 1 },
+		leave: { opacity: 0 }
+	});
 	return <div id="app">
 		<h2>App Here</h2>
 		<nav>
@@ -12,7 +20,11 @@ function App() {
 			<NavLink to="/chat">Chat</NavLink> |
 			<NavLink to="/animate">Animate</NavLink> |
 		</nav>
-		<Outlet />
+		{
+			transitions((style, item, t, key)=> (
+				<animated.div key={key} style={style}>{currentOutlet}</animated.div>
+			))
+		}
 	</div>
 }
 
